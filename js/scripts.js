b12f9db85bc2b3ad0919cd7b7ce3e9a86e9c3fe2
matt <thevetab@gmail.com>
2016-02-16 20:35:38 -0800

    makes vacation page outline

---
+++ js/scripts.js
@@ -0,0 +1,33 @@
+  $("form#worldview").submit(function(event) {
+
+  //   var age = $("select#age").val();
+
+  //   if (age === "yes" ) {
+  //       $("#age").confirm(conservative);
+  //     } else if (age === "no") 
+		// $("#age").confirm(liberal);
+  //     {
+
+  //   var racial_bias = $("select#racial_bias").val();
+
+  //   if (racial_bias === "yes" ) {
+  //       $("#racial_bias").confirm(liberal);
+  //     } else if (racial_bias === "no") 
+		// $("#racial_bias").confirm(conservative);
+  //     {
+
+  //   var liberal_conspiracy = $("select#liberal_conspiracy").val();
+
+  //   if (liberal_conspiracy === "yes" ) {
+  //       $("#liberal_conspiracy").confirm(liberal);
+  //     } else if (liberal_conspiracy === "no") 
+		// $("#liberal_conspiracy").confirm(conservative);
+  //     {
+
+  //   var conservative_fear = $("select#liberal_conspiracy").val();
+
+  //   if (conservative_fear === "yes" ) {
+  //       $("#conservative_fear").confirm(5);
+  //     } else if (conservative_fear === "no") 
+		// $("#conservative_fear").confirm(1);
+      {

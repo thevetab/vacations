4e0528e493527c97845edaa3fc7526386ea5b659
matt <thevetab@gmail.com>
2016-02-20 17:17:46 -0800

    debug jquery experiment

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  $("form#choices").submit(function(event) {
+    $("form#choices").submit(function(event) {
     
     var days = $("select#days").val();
     var nights = $("select#nights").val();
@@ -7,141 +7,58 @@ $(document).ready(function() {
     var dinner = $("select#dinner").val();
     var view = $("select#view").val();
 
-    if { 
-        ((days === "ak") && (nights === "ak" ) && (drinks === "ak")) ||
-        ((drinks === "ak") && (dinner === "ak") && (view === "ak")) ||
+    if  (((days === "nyc") && (nights === "nyc") && (drinks === "nyc")) ||
+        ((drinks === "nyc") && (dinner === "nyc") && (view === "nyc")) ||
+        ((nights === "nyc") && (drinks === "nyc") && (dinner === "nyc")) ||
+        ((days === "nyc") && (dinner === "nyc") && (view === "nyc")) ||
+        ((nights === "nyc") && (dinner === "nyc") && (view === "nyc")) ||
+        ((days === "nyc") && (nights === "nyc") && (view === "nyc")) ||
+        ((days === "nyc") && (dinner === "nyc") && (view === "nyc")) ||
+        ((days === "nyc") && (drinks === "nyc") && (dinner === "nyc")) ||
+        ((days === "nyc") && (nights === "nyc") && (dinner === "nyc")) ||
+        ((days === "nyc") && (drinks === "nyc") && (view === "nyc"))) {
+            $("#new_york").show();
+            $("#alaska").hide();
+            $("#fiji").hide();
+    } else if     
+        (((days === "ak") && (nights === "ak") && (drinks === "ak")) ||
         ((nights === "ak") && (drinks === "ak") && (dinner === "ak")) ||
-        ((days === "ak") && (drinks === "ak") && (view === "ak")) || 
-        ((views === "ak") && (days == "ak") && (nights === "ak")) 
-                    (#Alaska).show();
-                    (#New_York).hide();
-                    (#Fiji).hide();
-
-    } else if { 
-        ((days === "ny") && (nights === "ny" ) && (drinks === "ny")) ||
-        ((drinks === "ny") && (dinner === "ny") && (view === "ny")) ||
-        ((nights === "ny") && (drinks === "ny") && (dinner === "ny")) ||
-        ((days === "ny") && (drinks === "ny") && (view === "ny")) || 
-        ((views === "ny") && (days == "ny") && (nights === "ny")) 
-                    (#New_York).show();
-                    (#Alaska).hide();
-                    (#Fiji).hide();
-
-
-   } else {
-         ((days === "fj") && (nights === "fj" ) && (drinks === "fj")) ||
-        ((drinks === "fj") && (dinner === "fj") && (view === "fj")) ||
+        ((drinks === "ak") && (dinner === "ak") && (view === "ak")) ||
+        ((days === "ak") && (drinks === "ak") && (view === "ak")) ||
+        ((nights === "ak") && (dinner === "ak") && (view === "ak")) ||
+        ((days === "ak") && (dinner === "ak") && (view === "ak")) ||
+        ((days === "ak") && (nights === "ak") && (view === "ak")) ||
+        ((nights === "ak") && (drinks === "ak") && (view === "ak")) ||
+        ((days === "ak") && (drinks === "ak") && (dinner === "ak")) ||
+        ((days === "ak") && (nights === "ak") && (dinner === "ak"))) {
+            $("#alaska").show();
+            $("#fiji").hide();
+            $("#new_york").hide();
+    } else if 
+        (((days === "fj") && (nights === "fj") && (drinks === "fj")) ||
         ((nights === "fj") && (drinks === "fj") && (dinner === "fj")) ||
-        ((days === "fj") && (drinks === "fj") && (view === "fj")) || 
-        ((views === "fj") && (days == "fj") && (nights === "fj")) 
-                    (#Fiji).show();
-                    (#Alaska).hide();
-                    (#New_York).hide();
-        }
+        ((days === "fj") && (drinks === "fj") && (view === "fj")) ||
+        ((drinks === "fj") && (dinner === "fj") && (view === "fj")) ||
+        ((nights === "fj") && (dinner === "fj") && (view === "fj")) ||
+        ((days === "fj") && (dinner === "fj") && (view === "fj")) ||
+        ((days === "fj") && (nights === "fj") && (view === "fj")) ||
+        ((nights === "fj") && (drinks === "fj") && (view === "fj")) ||
+        ((days === "fj") && (drinks === "fj") && (dinner === "fj")) ||
+        ((days === "fj") && (nights === "fj") && (dinner === "fj"))) {
+            $("#fiji").show();
+            $("#alaska").hide();
+            $("new_york").hide();
 
+        } else {
+            $("#alaska").show();
+        }
+    
     event.preventDefault();
 
-
-    };
-
-
-
-
-
-
-
-
-
-
-
-
-//     var responses = [days, nights, drinks, dinner, view];
-
-//     var akTotal = 0;
-//     var nyTotal = 0; 
-//     var fjTotal = 0;
-
-//     var calculateTotal = function() {
-//     responses.forEach(function(response)) {
-//         if (response === 'ak') {
-//             akTotal += 1;
-//         } else if ( response === 'fj');
-//             fjTotal += 1;
-//         } else {
-//             nyTotal += 1;
-//         }
-//      })
-//         determineResult();
-//     };
-
-
-//     var determineResult = function() {
-//         if ((akTotal > fjTotal) && (akTotal > nyTotal)) {
-//             return "Alaska";
-//         } else if ((fjTotal > akTotal) && (fjTotal > nyTotal)) {
-//             return "Fiji";
-//         } else if ((nyTotal > akTotal) && (nyTotal > fjTotal)) {
-//             return "NYC";
-//         } else {
-//             return "Alaska";
-//         }
-//     })
-// };
-
-
-
-    // var quote = function() {
-    //     if ( akTotal > 3 ) {
-    //             (#Alaska).show();
-    //             (#New_York).hide();
-    //             (#Fiji).hide();
-
-    //     } else if ( fjTotal > 3 ) {
-    //             (#Fiji).show();
-    //             (#New_York).hide();
-    //             (#Alaska).hide();        
-
-    //     } else if ( nyTotal > 3 ) {
-    //             (#New_York).show();
-    //             (#Fiji).hide();
-    //             (#Alaska).hide();
-    //     } else {
-    //         return "Fuck it, you're going to Alaska."
-    //     })
-
-
-
-// $(generate).ready(function() {
-//   $("form#choices").submit(function(event) {
+      });      
+    });
     
-//     var days = $("select#days").val();
-//     var nights = $("select#nights").val();
-//     var drinks = $("select#drinks").val();
-//     var dinner = $("select#dinner").val();
-//     var view = $("select#view").val();
+   
 
-//     var responses = [days, nights, drinks, dinner, view];
-
-//     var ak
-
-
-
-
-
-
-    // var dinner = $("select#dinner").val();
-    // var view = $("select#view").val();
-
-    // var quote = (ak + nights + drinks + dinner + view);
-    // if (days === "AK" && nights === "AK" && drinks === "AK" && drinks === "AK") {
-    //   quote = Alaska;
-    // }
-
-    // $("#rate").text(quote);
-    // $("#quote").show()
-
-    // event.preventDefault();
-
-
-
-
+  
+ 
